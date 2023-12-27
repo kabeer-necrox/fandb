f44a53b1114a71ed475b00b662311ac077e7b3c2
kabeer-necrox <khushkabeer@gmail.com>
2023-12-27 14:06:39 +0300

    here is the local serve and code

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = [
+    {name:"kabeer",
+    age: 23,
+    email: "kabeer@gmail.com",
+},
+
+{name:"ali",
+age: 43,
+email: "ali@gmail.com",
+},
+{name:"baig",
+age: 23,
+email: "baiggmail.com",
+},
+{name:"nexrox",
+age: 29,
+email: "necrox@gmail.com",
+},
+]
+
+
+module.exports = data;
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const http = require("http")
+const http = require("http");
+const data = require("./data");
+const { log } = require("console");
 
 const port = 4000;
-http.createServer((req,resp)=>{
-resp.write(`welcome to local host:${port}`);
-resp.end()
-}).listen(port)
+
+http.createServer((req, resp) => {
+  resp.writeHead(200, { "content-type": "application/json" });
+
+  resp.write(JSON.stringify(data));
+  resp.end();
+}).listen(port);
+console.log(data)
+
