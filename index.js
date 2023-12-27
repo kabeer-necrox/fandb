edae0095780feb407fb04a55b302fb9a2acb1009
kabeer-necrox <khushkabeer@gmail.com>
2023-12-27 15:18:36 +0300

    express js code

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const http = require("http");
-const data = require("./data");
-const { log } = require("console");
+// const http = require("http");
+// const data = require("./data");
+// const { log } = require("console");
 
-const port = 4000;
+// const port = 4000;
 
-http.createServer((req, resp) => {
-  resp.writeHead(200, { "content-type": "application/json" });
+// http.createServer((req, resp) => {
+//   resp.writeHead(200, { "content-type": "application/json" });
 
-  resp.write(JSON.stringify(data));
-  resp.end();
-}).listen(port);
-console.log(data)
+//   resp.write(JSON.stringify(data));
+//   resp.end();
+// }).listen(port);
+// console.log(data)
+
+
+const express = require('express');
+const app = express()
+
+app.get("/about",(req,resp)=>{
+  resp.send("local server is running about")
+})
+
+app.get("/",(req,resp)=>{
+  resp.send("local server is running")
+})
+app.listen(8000,()=>{
+  console.log("local host is running");
+})
 
